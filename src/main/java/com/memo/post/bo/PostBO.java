2817c569ecfcdbf1b38edd46887f1390a6b20484
DESKTOP-QAGTK6B\user <test@test.com>
2024-07-16 18:50:32 +0900

    240716 글쓰기 목록 화면

---
+++ src/main/java/com/memo/post/bo/PostBO.java
@@ -0,0 +1,29 @@
+package com.memo.post.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.memo.post.domain.Post;
+import com.memo.post.mapper.PostMapper;
+
+@Service
+public class PostBO {
+	
+	@Autowired
+	private PostMapper postMapper;
+	
+	// input: 로그인 된 사람의 userId
+	// output: List<Post>
+	public List<Post> getPostListByUserId(int userId) {
+		return postMapper.selectPostListByUserId(userId);
+	}
+	
+	// input: 3개의 파라미터
+	// output: 
+	public List<Post> addPostList(String subject, String content, String file){
+		return postMapper.insertPost
+	}
+	
+}
--- src/main/java/com/memo/post/mapper/PostMapper.java
+++ src/main/java/com/memo/post/mapper/PostMapper.java
@@ -4,10 +4,20 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.memo.post.domain.Post;
 
 @Mapper
 public interface PostMapper {
 	
+	// test용
 	public List<Map<String, Object>> selectPostListTest();
 	
+	public List<Post> selectPostListByUserId(int userId);
+	
+	public List<Post> insertPostList(
+			@Param("subject") String subject, 
+			@Param("content") String content, 
+			@Param("file") String file);
 }
