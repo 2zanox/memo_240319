9e6a6ab45d4fae34e3b5ca820de81bb7d0e3de9c
DESKTOP-QAGTK6B\user <test@test.com>
2024-08-06 17:17:47 +0900

    Junit test

---
+++ src/test/java/com/memo/user/bo/UserBOTest.java
@@ -0,0 +1,14 @@
+package com.memo.user.bo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserBOTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
